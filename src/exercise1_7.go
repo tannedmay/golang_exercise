5a23a8737b0099fb5760306e07ed601e10eca963
tannedmay <mail@tannedmay.me>
2019-09-08 22:40:38 +0530

    Added 1.7-1.9

---
+++ src/exercise1_7.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %s", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in reading response: %s", err)
+			os.Exit(1)
+		}
+	}
+}
